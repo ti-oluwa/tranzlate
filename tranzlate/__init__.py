efdd5397008034b2c947d4e971243d2565d52f3d
ti-oluwa <105179638+ti-oluwa@users.noreply.github.com>
2024-08-17 11:17:50 +0100

    Update

--- tranzlate/__init__.py
+++ tranzlate/__init__.py
@@ -2,11 +2,9 @@
 #### tranzlate
 
 Translate text, files, markup and BeautifulSoup.
-
-@Author: Daniel T. Afolayan (ti-oluwa.github.io)
 """
 
 from .translator import Translator, add_translatable_html_tag
 
 __all__ = ["Translator", "add_translatable_html_tag"]
-__version__ = "0.0.4"
+__version__ = "1.0.0"
